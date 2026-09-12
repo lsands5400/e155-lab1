11fa9014c4888df664483385410e3f6f4597b643
lsands5400 <lsands@g.hmc.edu>
2026-09-11 22:36:55 -0700

    Refactor counterLED module for LED control
    Updated counterLED module to change parameters and logic for LED blinking.

--- counterLED.sv
+++ counterLED.sv
@@ -4,24 +4,39 @@
 // This is the counter module for E155 Lab 1. It blinks an LED at 2.4Hz. 
 // The code is based on the demo at 
 // https://hmc-e155.github.io/tutorials/tutorial-posts/lattice-radiant-ice40-ultraplus-project-setup/
-module counterLED #(parameter width = 25,
-					parameter max = 24'd10_000_000)
-					(input logic int_osc, reset, en,
-					 output logic led);
-					 
-	logic [width-1:0] counter; 
-	// Counter
-	always_ff@(posedge int_osc, negedge reset) begin
-		// Reset count if reset button pushed or count gets to maximum value
-		// Reset is active LOW
-		if((reset == 0)||(counter == max-1)) counter <= 0;
-			
-		// Increment counter the same if enable is on		
-		else if(en)		   				  counter <= counter + 1;
-		else							  counter <= counter;
-	end
-	
-	// Assign LED output
-	assign led = counter[width-1];
+module counterLED #(
+    parameter int width = 23,
+    parameter int max   = 5_000_000
+)(
+    input  logic int_osc,
+    input  logic reset,
+    input  logic en,
+    output logic led
+);
+
+    logic [width-1:0] counter;
+
+    // Counter
+    always_ff @(posedge int_osc, negedge reset) begin
+
+        // Reset counter and LED when reset is pressed
+        // Reset is active LOW
+        if (!reset) begin
+            counter <= '0;
+            led     <= 1'b0;
+        end
+
+        // Toggle LED every 5,000,000 clock cycles
+        else if (en) begin
+            if (counter == max - 1) begin
+                counter <= '0;
+                led     <= ~led;
+            end
+            else begin
+                counter <= counter + 1'b1;
+            end
+        end
+
+    end
 
 endmodule
